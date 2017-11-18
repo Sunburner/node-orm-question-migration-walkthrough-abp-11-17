c8bac86e56dbf1368729cbd669c82b658fe1b6ea
Angelo Cigna <acigna1982@gmail.com>
2017-11-18 20:27:18 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -6,7 +6,10 @@ class Question{
  static CreateTable() {
   return new Promise(function(resolve){
     resolve("This Does Nothing!")
-
+      const sql = 'CREATE TABLE questions (id INTEGER PRIMARY KEY)'
+      db.run(sql,function(){
+        resolve("questions table created")
+      })
   })
 }
 
